30f2f6f576bc2da13ae2b9cff2be256b98d37000
0xpantera <0xpantera@proton.me>
2023-04-10 18:04:40 -0600

    call game2 contract to win

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,5 @@
 use bindings::game_1::Game1;
+use bindings::game_2::Game2;
 
 use ethers::{types::Address};
 use ethers::providers::{Provider, Http, Middleware};
@@ -9,11 +10,41 @@ use std::sync::Arc;
 #[tokio::main]
 async fn main() -> Result<()> {
     let rpc_url = "http://localhost:8545";
-    let address: Address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".parse()?;
+    let sender: Address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".parse()?;
+    
+    call_game1(rpc_url, sender).await?;
+    call_game2(rpc_url, sender).await?;
+
+    Ok(())
+}
+
+async fn call_game2(rpc_url: &str, sender: Address) -> Result<()> {
+    let contract_address: Address = "0x8a791620dd6260079bf849dc5567adc3f2fdc318".parse()?;
+
+    let prov = Provider::<Http>::try_from(rpc_url)?;
+    let provider = Arc::new(prov.with_sender(sender));
+
+    let contract = Game2::new(contract_address, provider.clone());
+
+    let call_setx = contract.set_x(30.into());
+    let setx_tx = call_setx.send().await?.log_msg("Setting x");
+
+    let call_sety = contract.set_y(20.into());
+    let sety_tx = call_sety.send().await?.log_msg("Setting y");
+
+    let call_win = contract.win();
+    let win_tx = call_win.send().await?;
+    println!("RECEIPT:\n {:?}", win_tx);
+
+
+    Ok(())
+}
+
+async fn call_game1(rpc_url: &str, sender: Address) -> Result<()> {
     let contract_address: Address = "0xa513e6e4b8f2a923d98304ec87f64353c4d5c853".parse()?;
 
     let prov = Provider::<Http>::try_from(rpc_url)?;
-    let provider = Arc::new(prov.with_sender(address));
+    let provider = Arc::new(prov.with_sender(sender));
 
     println!("SENDER ADDRESS: \n{:?}", provider.default_sender().unwrap());
     let contract = Game1::new(contract_address, provider.clone());
