186f40a4d7f3540d3e3bfbd656968421bf549601
0xpantera <0xpantera@proton.me>
2023-04-10 18:57:59 -0600

    Add game3 deployment script

--- contracts/script/Games.s.sol
+++ contracts/script/Games.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.17;
 import "forge-std/Script.sol";
 import "../src/Game1.sol";
 import "../src/Game2.sol";
+import "../src/Game3.sol";
 
 contract Game1Script is Script {
     function setUp() public {}
@@ -23,6 +24,17 @@ contract Game2Script is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         Game2 game = new Game2();
+        vm.stopBroadcast();
+    }
+}
+
+contract Game3Script is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        Game3 game = new Game3();
         vm.stopBroadcast();
     }
 }
